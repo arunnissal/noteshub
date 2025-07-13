5d1d6f783ff442cc78d2edd40d80de30c1dbe75d
arunnissal <arunnissal45@gmail.com>
2025-07-13 12:15:23 +0530

    Use environment variables for production config

--- noteshub/settings.py
+++ noteshub/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_yga@&^qm^p*qq9@)$g4je*$#a+43p&0d!ghj8rc42b&w5pvoy'
+SECRET_KEY = os.environ.get("SECRET_KEY", "unsafe-default")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost").split(",")
 
 
 # Application definition
@@ -81,9 +81,7 @@ WSGI_APPLICATION = 'noteshub.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='postgresql://noteshub_db_user:UmWh70tfSqy6FztcFu7SKGXWTVnbMztL@dpg-d1pkri2dbo4c73bm2s1g-a.oregon-postgres.render.com/noteshub_db'
-    )
+    'default': dj_database_url.config(default=os.environ.get("DATABASE_URL"))
 }
 
 
